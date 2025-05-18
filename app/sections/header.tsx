e39f68b396d9bdcacc410c6d79ad38aeba68fc5c
Korvo2Tuirow <korvo2irow@live.com>
2025-05-17 21:05:09 -0300

    css responsivo do header e logo nova

--- app/sections/header.tsx
+++ app/sections/header.tsx
@@ -1,32 +1,25 @@
-
-import Image from "next/image"
-import logo from "@/public/image/NewLogoPdC.png"
+import Image from "next/image";
+import logo from "@/public/image/NewLogoPdC.png";
 
 export const Header = () => {
   return (
     <>
-      <div className="relative flex  w-full h-[600px] overflow-hidden ">
-        <video
-          className="absolute -top-15  items-center w-full h-full object-cover"
-          src="https://pointdocanal.vercel.app/PDCvideo.mp4"
-          autoPlay
-          loop
-          muted
-        />
+      <div className="flex  w-full h-[550px] overflow-hidden ">
 
-          <div  className="absolute flex  w-full z-10  object-cover">
-            <Image
-              src={logo}
-              alt="Logo"
-              width={550}
-              height={550}
-             
-            
-            />
-          </div>
+        <div className="relative w-screen h-full z-0 -top-25">
+          <video
+            src="https://pointdocanal.vercel.app/PDCvideo.mp4"
+            autoPlay
+            loop
+            muted
+            className="w-screen justify-center object-cover h-250"
+          />
+        </div>
 
-       
-        
+        <div className="absolute flex items-center justify-center w-full h-[550px] z-10  object-cover ">
+          <Image src={logo} alt="Logo" width={500} height={500} />
+        </div>
+
       </div>
     </>
   );
