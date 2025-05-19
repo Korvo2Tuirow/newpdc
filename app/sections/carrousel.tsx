b9cd197011fc783cada3d5e40075a32844f88f65
Korvo2Tuirow <korvo2irow@live.com>
2025-05-18 21:45:50 -0300

    scale

--- app/sections/carrousel.tsx
+++ app/sections/carrousel.tsx
@@ -13,7 +13,7 @@ import h4img from "@/public/image/imgCarrousel/h04.jpg";
 
 export const Carrousel =() => {
   const [activeIndex, setActiveIndex] = useState(2); // Começa no meio
-  const imagens = [h1img, h2img, h3img, h4img];
+  const imagens = [h1img, h2img, h3img, h4img, h1img, h2img, h3img, h4img ];
   const handlePrev = () => {
     setActiveIndex((prev) => (prev === 0 ? imagens.length - 1 : prev - 1));
   };
@@ -24,7 +24,7 @@ export const Carrousel =() => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center bg-gray-300 py-10">
-      <div className="relative w-screen max-w-[1000px] h-96 flex items-center justify-center overflow-hidden">
+      <div className="relative w-screen max-w-[1000px] h-110 flex items-center justify-center overflow-hidden">
         {imagens.map((src, index) => {
           const offset = index - activeIndex;
 
@@ -32,7 +32,7 @@ export const Carrousel =() => {
           const isActive = offset === 0;
 
           const transform = isActive
-            ? 'scale-110 z-30'
+            ? 'scale-130 z-30'
             : offset === -1
             ? '-translate-x-80 rotate-y-5 scale-90 z-20'
             : offset === 1
@@ -54,13 +54,13 @@ export const Carrousel =() => {
       <div className="flex gap-6 mt-6">
         <button
           onClick={handlePrev}
-          className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-300 transition"
+          className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-500 transition"
         >
       
         </button>
         <button
           onClick={handleNext}
-          className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-300 transition"
+          className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-500 transition"
         >
           
         </button>
