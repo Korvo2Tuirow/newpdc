83182df5c5e868216f8dc7b118ba58fac82342a4
Korvo2Tuirow <korvo2irow@live.com>
2025-05-19 14:56:11 -0300

    touch no carrousel

--- app/sections/carrousel.tsx
+++ app/sections/carrousel.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-
+import { useRef, useState } from "react";
 
 import Image from "next/image";
 
 import h2img from "@/public/image/imgCarrousel/h03.jpg";
 import h4img from "@/public/image/imgCarrousel/hamb01.jpg";
-import h5img from"@/public/image/imgCarrousel/hamb02.jpg"
+import h5img from "@/public/image/imgCarrousel/hamb02.jpg";
 import comb01 from "@/public/image/imgCarrousel/combo01.jpg";
- 
 
-
-export const Carrousel =() => {
+export const Carrousel = () => {
   const [activeIndex, setActiveIndex] = useState(2); // Começa no meio
-  const imagens = [  comb01, h4img, h5img, h2img]; 
+  const startX = useRef<number | null>(null);
+
+  const imagens = [comb01, h4img, h5img, h2img];
+
   const handlePrev = () => {
     setActiveIndex((prev) => (prev === 0 ? imagens.length - 1 : prev - 1));
   };
@@ -23,23 +23,62 @@ export const Carrousel =() => {
     setActiveIndex((prev) => (prev === imagens.length - 1 ? 0 : prev + 1));
   };
 
+  const handleTouchStart = (e: React.TouchEvent) => {
+    startX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    const endX = e.changedTouches[0].clientX;
+    if (startX.current !== null) {
+      const diff = startX.current - endX;
+
+      if (diff > 50) {
+        handleNext();
+      } else if (diff < -50) {
+        handlePrev();
+      }
+    }
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    startX.current = e.clientX;
+  };
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    const endX = e.clientX;
+    if (startX.current !== null) {
+      const diff = startX.current - endX;
+
+      if (diff > 50) {
+        handleNext();
+      } else if (diff < -50) {
+        handlePrev();
+      }
+    }
+  };
 
   return (
     <div className="w-full flex flex-col items-center justify-center bg-gray-300 py-10">
-      <div className="relative w-screen max-w-[1000px] h-110 flex items-center justify-center overflow-hidden">
+      <div
+        className="relative w-screen max-w-[1000px] h-110 flex items-center justify-center overflow-hidden"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
         {imagens.map((src, index) => {
           const offset = index - activeIndex;
 
-          const baseStyle = 'absolute transition-all duration-500 ease-in-out';
+          const baseStyle = "absolute transition-all duration-500 ease-in-out";
           const isActive = offset === 0;
 
           const transform = isActive
-            ? 'scale-130 z-30'
+            ? "scale-130 z-30"
             : offset === -1
-            ? '-translate-x-80 rotate-y-5 scale-90 z-20'
+            ? "-translate-x-80 rotate-y-5 scale-90 z-20"
             : offset === 1
-            ? 'translate-x-80 -rotate-y-5 scale-90 z-20'
-            : 'opacity-0 scale-75 z-10';
+            ? "translate-x-80 -rotate-y-5 scale-90 z-20"
+            : "opacity-0 scale-75 z-10";
 
           return (
             <Image
@@ -47,6 +86,7 @@ export const Carrousel =() => {
               src={src}
               alt={`Slide ${index}`}
               className={`${baseStyle} ${transform}  w-[300px] h-[300px] object-cover shadow-lg shadow-black/50 rounded-md`}
+              draggable="false"
             />
           );
         })}
@@ -57,16 +97,12 @@ export const Carrousel =() => {
         <button
           onClick={handlePrev}
           className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-500 transition"
-        >
-      
-        </button>
+        ></button>
         <button
           onClick={handleNext}
           className="bg-gray-700 text-black px-4 py-2 rounded-full hover:bg-gray-500 transition"
-        >
-          
-        </button>
+        ></button>
       </div>
     </div>
   );
-}
+};
