36633eff17d6ca22f7aa77b868a7363b82412a74
Korvo2Tuirow <korvo2irow@live.com>
2025-05-20 12:38:36 -0300

    componets

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,14 @@ import { Header } from "./sections/header";
 
 import { Carrousel } from "./sections/carrousel";
 import { Comments } from "./sections/comments";
+import { WelcomeBanner } from "./sections/welcomeBanner";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <div className="flex flex-col items-center justify-center ">
-        <div className="p-8 mt-5 text-3xl font-bold flex justify-center text-center  w-screen max-w-[1000px] text-gray-700">
-          🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!
-        </div>
+      <Header /> 
 
-        <div className="p-8 -mt-8 flex  w-screen text-2xl text-center text-gray-600 w-screen  max-w-[1200px]">
-          Se você busca hambúrgueres suculentos com pão de fabricação própria,
-          pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para
-          refrescar o dia, você está no lugar certo! 😍 Aqui, cada detalhe faz a
-          diferença. Nosso pão é artesanal, feito diariamente para garantir
-          frescor e sabor incomparáveis. Cada ingrediente é escolhido com
-          carinho, resultando em uma experiência gastronômica única e
-          inesquecível. 📍 Peça agora e descubra o verdadeiro sabor! 🚀 Delivery
-          rápido | 🏠 Ambiente aconchegante | ❤️ Feito com paixão 🔽 Faça seu
-          pedido agora mesmo!
-        </div>
-      </div>
+      <WelcomeBanner />  
 
       <Carrousel />
 
---
+++ app/sections/welcomeBanner.tsx
@@ -0,0 +1,22 @@
+
+export const WelcomeBanner = () => {
+
+    return(
+           <div className="flex flex-col items-center justify-center my-5 ">
+        <div className="p-8 mt-5 text-4xl font-bold flex justify-center text-center  w-screen max-w-[1000px] text-gray-700 ">
+          🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!
+        </div>
+
+        <div className="p-8  flex  w-screen text-2xl text-center text-gray-600 w-screen  max-w-[1000px]">
+          Se você busca hambúrgueres suculentos com pão de fabricação própria,
+          pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para
+          refrescar o dia, você está no lugar certo! Aqui, cada detalhe faz a
+          diferença. Nosso pão é artesanal, feito diariamente para garantir
+          frescor e sabor incomparáveis. Cada ingrediente é escolhido com
+          carinho, resultando em uma experiência gastronômica única e
+          inesquecível. Peça agora e descubra o verdadeiro sabor. ❤️ Feito com paixão. 🚀 Delivery
+          rápido, faça seu pedido agora mesmo!
+        </div>
+      </div>
+    )
+}
