a3c02e0cc04c66c4c6e43c0c15a1d7775cd3c287
Korvo2Tuirow <korvo2irow@live.com>
2025-05-21 03:08:04 -0300

    shadow h1

--- app/sections/welcomeBanner.tsx
+++ app/sections/welcomeBanner.tsx
@@ -3,11 +3,11 @@ export const WelcomeBanner = () => {
 
     return(
            <div className="flex flex-col items-center justify-center my-5 ">
-        <div className="p-8 mt-5 text-4xl font-bold flex justify-center text-center  w-screen max-w-[1000px] text-gray-700 ">
+        <div className="p-8 mt-5 text-4xl font-bold flex justify-center text-center  w-screen max-w-[1000px] text-gray-700 text-shadow-lg/10 ">
           🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!
         </div>
 
-        <div className="p-8  flex  w-screen text-2xl text-center text-gray-600 w-screen  max-w-[1000px]">
+        <div className="p-8  flex text-indent text-2xl text-center text-gray-600 w-screen  max-w-[1000px] ">
           Se você busca hambúrgueres suculentos com pão de fabricação própria,
           pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para
           refrescar o dia, você está no lugar certo! Aqui, cada detalhe faz a
